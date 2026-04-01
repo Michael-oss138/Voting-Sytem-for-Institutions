96573570c6edd037f2ed057a1609f8adf56f4a54
Michael-oss138 <nwaizumichael0@gmail.com>
2026-04-01 18:56:31 +0100

    done

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('student', 'Student'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    matric_no = models.CharField(max_length=20, blank=True, null=True)
+    department = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.username
---
+++ myapp/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import User
+
+class AdminRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            role='admin'
+        )
+        return user
