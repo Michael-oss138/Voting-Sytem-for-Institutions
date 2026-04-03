e96ba19c2576d5d6689b2e94f713704e39a5f549
Michael-oss138 <nwaizumichael0@gmail.com>
2026-04-03 22:51:40 +0100

    Election model

--- myapp/models.py
+++ myapp/models.py
@@ -20,3 +20,23 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Election(models.Model):
+    STATUS_CHOICES =(
+        ('draft': 'Draft'),
+        ('opened': 'Opened'),
+        ('closed': 'Closed')
+    )
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
